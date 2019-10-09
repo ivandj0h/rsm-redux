e998e22ba2fda290a64cb5e043328b60ce88b67b
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-09 14:57:46 +0800

    modified readme and added navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment }from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 
@@ -6,9 +6,10 @@ function App() {
   return (
     <Fragment>
       <Navbar />
-    <div className="container">
-      <h3>Welcomet to REACT- REDUX</h3>
-    </div>
+      <div className="container">
+        <br />
+        <h3>Welcome...</h3>
+      </div>
     </Fragment>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,28 +4,10 @@ export default function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
+                <a className="navbar-brand" href="#">REACT- REDUX</a>
                 <div className="collapse navbar-collapse" id="navbarColor01">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                    </ul>
-  </div>
-</nav>
+                </div>
+            </nav>
         </div>
-            )
-        }
+    )
+}
